45be0310ef947a6d5746a8d00346dc5d5ca43b49
michel2407 <62596237+michel2407@users.noreply.github.com>
2020-04-30 10:28:06 +0200

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-var lastName = ' Nom: Doe';
-var espace =' '
-var firstName = 'Prénom: John';
-var espace = ' '
-var city= 'Ville: New York';
-var result = lastName + espace + firstName + espace + city;
-alert(result);
