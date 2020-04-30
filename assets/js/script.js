1bf3fea73a20f25745b103bd98434b632241b496
michel2407 <62596237+michel2407@users.noreply.github.com>
2020-04-30 10:26:21 +0200

    ex2 Js Corriger

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,8 @@
-var lastName = ' Nom: Doe';
-var espace =' '
-var firstName = 'Prénom: John';
-var espace = ' '
-var city= 'Ville: New York';
-var result = lastName + espace + firstName + espace + city;
-alert(result);
+var lastName = 'Doe';
+var firstName = 'John';
+var city= 'New York';
+alert('Nom : ' + lastName + '\n' + 'Prénom : ' + firstName + '\n'  + 'Ville : ' + city);
+//alert('Je m\'appelle ' + firstName);
+// pour faire un espace, mettre un espace avant la fermeture de la côte
+// pour faire un retour de ligne il faut utilser \n dans une entre-côte pour qu'il soit considéré comme un string.
+// pour échaper une une côte, il faut utilser le : \
