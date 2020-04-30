fd7e2893aca4c44f4ba99ecbf33f9e2982bb3ba8
michel2407 <62596237+michel2407@users.noreply.github.com>
2020-04-30 08:55:10 +0200

    ex2_modifierNonCorriger

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,7 @@
-var lastName = 'Doe', firstName = 'John', city= 'New York', "result";
-result = lastName + firstName + city;
+var lastName = ' Nom: Doe';
+var espace =' '
+var firstName = 'Prénom: John';
+var espace = ' '
+var city= 'Ville: New York';
+var result = lastName + espace + firstName + espace + city;
 alert(result);
